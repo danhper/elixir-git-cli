3a5468c03fb0259bf935708116a3ece83c53a032
Daniel Perez <daniel@claudetech.com>
2016-07-18 01:01:41 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Git.Mixfile do
 
   def project do
     [app: :git_cli,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.0",
      name: "git_cli",
      source_url: "https://github.com/tuvistavie/elixir-git-cli",
